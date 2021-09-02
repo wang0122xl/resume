c13454a89165eb6500911d6850d7a982b1eb2aef
喵大快救我 <wang0122xl@163.com>
2021-09-02 14:10:51 +0800

    :sparkles: update skills

--- src/components/skill.tsx
+++ src/components/skill.tsx
@@ -2,7 +2,7 @@
  * @Date: 2021-09-01 23:26:36
  * @Author: wang0122xl@163.com
  * @LastEditors: wang0122xl@163.com
- * @LastEditTime: 2021-09-01 23:38:19
+ * @LastEditTime: 2021-09-02 14:09:41
  * @Description: file content
  */
 
@@ -11,11 +11,14 @@ const Skill = () => {
         <>
         <h2>专业技能</h2>
         <section>
-            <p>熟练掌握html, javascript/typescript, css。可独立完成前端项目开发工作</p>
-            <p>熟练掌握Objective-C, 擅长制作易用高性能的UI组件，可独立负责iOS项目的开发 - 上线任务</p>
-            <p>熟练使用nodejs，可承担部分全栈工作</p>
-            <p>熟练使用linux系统，熟悉nginx常用配置</p>
-            <p>熟悉docker</p>
+            <p>熟练掌握 html、javascript/typescript、css。可独立负责前端项目开发(web, h5, 小程序)</p>
+            <p>熟练掌握 React、Vue 等常用前端框架, 结合Context/Redux、Vuex快速构建项目</p>
+            <p>熟练使用前端开发构建工具 Webpack/Vite, 并了解其常用优化方案 </p>
+            <p>熟练掌握 Objective-C, 擅长 H5, react-native 混合App 方案，可独立开发上线功能完善的混合App。了解Flutter</p>
+            <p>熟练使用Nodejs，可承担部分服务端开发工作</p>
+            <p>熟练使用Linux系统，熟悉Nginx常用配置</p>
+            <p>熟悉Docker</p>
+            <p>熟练使用git管理代码</p>
         </section>
         </>
     )
