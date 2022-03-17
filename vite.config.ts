9b5005ad6a6dfe3f811a12137a1471ab9606c6f8
喵大快救我 <wang0122xl@163.com>
2022-03-17 17:10:04 +0800

    :sparkles: change output dir

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Date: 2021-08-31 19:40:51
  * @Author: wang0122xl@163.com
  * @LastEditors: wang0122xl@163.com
- * @LastEditTime: 2022-03-17 17:07:37
+ * @LastEditTime: 2022-03-17 17:09:36
  * @Description: file content
  */
 import { defineConfig } from "vite";
@@ -14,7 +14,7 @@ import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 export default defineConfig({
     base: '/resume/',
     build: {
-        outDir: 'docs',
+        outDir: 'dist',
     },
     server: {
         host: '0.0.0.0',
