6770da3a14f3dd78aae05b82effecfb0ce651adc
喵大快救我 <wang0122xl@163.com>
2022-03-17 17:08:09 +0800

    :green_heart: ci build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Date: 2021-08-31 19:40:51
  * @Author: wang0122xl@163.com
  * @LastEditors: wang0122xl@163.com
- * @LastEditTime: 2021-09-02 00:07:58
+ * @LastEditTime: 2022-03-17 17:07:37
  * @Description: file content
  */
 import { defineConfig } from "vite";
@@ -12,7 +12,7 @@ import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    base: '/resume/',
     build: {
         outDir: 'docs',
     },
