ceeb35ab40ca4c720a79a7e893298e94bc665276
wangxiaolong <wang0122xl@163.com>
2023-07-04 13:43:44 +0800

    :speech_balloon: skills & evaluation

--- src/components/evaluation.tsx
+++ src/components/evaluation.tsx
@@ -2,7 +2,7 @@
  * @Date: 2021-09-01 23:28:06
  * @Author: wang0122xl@163.com
  * @LastEditors: wang0122xl@163.com
- * @LastEditTime: 2021-09-02 21:30:27
+ * @LastEditTime: 2023-07-04 13:42:10
  * @Description: file content
  */
 
@@ -11,7 +11,8 @@ const Evaluation = () => {
         <>
         <h2>自我评价</h2>
         <section>
-            <p>适应、学习、抗压能力极强</p>
+            <p>native, web, hybrid 均可独立完成项目开发</p>
+            <p>学习、抗压能力极强</p>
             <p>轻度代码洁癖，对代码质量有严格的要求</p>
         </section>
         </>
--- src/components/skill.tsx
+++ src/components/skill.tsx
@@ -2,7 +2,7 @@
  * @Date: 2021-09-01 23:26:36
  * @Author: wang0122xl@163.com
  * @LastEditors: wang0122xl@163.com
- * @LastEditTime: 2023-03-10 17:44:46
+ * @LastEditTime: 2023-07-04 13:43:17
  * @Description: file content
  */
 
@@ -14,10 +14,10 @@ const Skill = () => {
             <p>熟练掌握 html、javascript/typescript、css。可独立负责前端项目开发(WEB, H5, 小程序)</p>
             <p>熟练掌握 React、Vue 等常用前端框架, 与其相应的配套开发库</p>
             <p>熟练使用前端开发构建工具 Webpack/Vite, 并了解其常用优化方案 </p>
-            <p>熟练掌握 Objective-C, 擅长 H5, react-native 等混合App方案，可独立开发上线功能完善的混合App。了解Flutter</p>
+            <p>熟练掌握 Objective-C, 擅长 H5, react-native 等混合App方案，可独立开发上线功能完善的混合App。</p>
             <p>熟练使用Nodejs</p>
             <p>熟练使用Linux系统，熟悉Nginx常用配置</p>
-            <p>熟练使用Docker</p>
+            <p>熟练掌握CI/CD相关技能</p>
             <p>熟练使用Git管理代码</p>
         </section>
         </>
