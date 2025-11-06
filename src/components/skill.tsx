ab4b359a556981ecd06a944711c2a0c96efd62f9
王晓龙 <wang0122xl@163.com>
2025-11-06 16:58:50 +0800

    Update sections to use 'nomrgin' class

--- src/components/skill.tsx
+++ src/components/skill.tsx
@@ -10,28 +10,28 @@ const Skill = () => {
     return (
         <>
         <h2>专业技能</h2>
-            <section>
+            <section class='nomrgin'>
                 <p>熟练掌握 html、javascript/typescript、css。可独立负责前端项目开发(WEB, H5, 小程序，原生iOS)</p>
             </section>
-            <section>
+            <section class='nomrgin'>
                 <p>熟练掌握 React、Vue 等常用前端框架, 与其相应的配套开发库</p>
             </section>
-            <section>
+            <section class='nomrgin'>
                 <p>熟练使用前端开发构建工具 Webpack/Vite, 及常用优化方案 </p>
             </section>
-            <section>
+            <section class='nomrgin'>
                 <p>熟练掌握 Objective-C, 擅长 H5, react-native 等混合App方案，可独立开发上线功能完备的原生/混合App。</p>
             </section>
-            <section>
+            <section class='nomrgin'>
                 <p>熟练使用puppeteer、cypress完成项目的自动化测试</p>
             </section>
-            <section>
+            <section class='nomrgin'>
                 <p>熟练使用cursor等ai工具提升工作效率</p>
             </section>
-            <section>
+            <section class='nomrgin'>
                 <p>熟练使用Linux系统，熟悉Nginx常用配置</p>
             </section>
-            <section>
+            <section class='nomrgin'>
                 <p>熟练掌握CI/CD相关技能</p>
             </section>
             <section>
